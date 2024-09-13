53fcecd513e9410074ced55391733b645226a925
Yura-Moroz <outlanderxl29@gmail.com>
2024-09-13 23:07:07 +0300

    first_commit

--- src/main/java/com/game/entity/Player.java
+++ src/main/java/com/game/entity/Player.java
@@ -1,23 +1,40 @@
 package com.game.entity;
 
+import jakarta.persistence.*;
+
 import java.util.Date;
 
+@org.hibernate.annotations.NamedQuery(name="getAllCountQuery", query="SELECT COUNT(p) FROM Player p")
 
+@Entity
+@Table(schema = "rpg", name="player")
 public class Player {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(nullable = false)
     private Long id;
 
+    @Column(length = 12, nullable = false)
     private String name;
 
+    @Column(length = 30, nullable = false)
     private String title;
 
+    @Column(nullable = false)
+    @Enumerated(EnumType.ORDINAL)
     private Race race;
 
+    @Column(nullable = false)
+    @Enumerated(EnumType.ORDINAL)
     private Profession profession;
 
+    @Column(nullable = false)
     private Date birthday;
 
+    @Column(nullable = false)
     private Boolean banned;
 
+    @Column(nullable = false)
     private Integer level;
 
     public Player() {
